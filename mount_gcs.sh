1b3d2f190e759226e0e371bc2dcab25609953358
Yi Yang <yiya@google.com>
2026-08-03 22:54:35 +0000

    fix mount_gcs.sh

--- mount_gcs.sh
+++ mount_gcs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Mount GCS buckets for DROID pipeline
+sudo modprobe fuse
 
-umount ~/droid_data/input/robotics/droid_raw
+fusermount -uz ~/droid_data/input/robotics/droid_raw 2>/dev/null
 mkdir -p ~/droid_data/input/robotics/droid_raw
 gcsfuse --implicit-dirs --only-dir robotics/droid_raw gresearch ~/droid_data/input/robotics/droid_raw
 
-umount ~/droid_data/output/mv-tap
+fusermount -uz ~/droid_data/output/mv-tap 2>/dev/null
 mkdir -p ~/droid_data/output/mv-tap
 gcsfuse --implicit-dirs --only-dir mv-tap dm-tapnet ~/droid_data/output/mv-tap
